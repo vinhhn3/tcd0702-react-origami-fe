1cb121dc954d33de31a753278550741468fcad77
Vinh Hoang <vinhhn3@fe.edu.vn>
2023-11-27 15:26:33 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
-import {
-  Route,
-  BrowserRouter as Router,
-  Switch,
-} from "react-router-dom/cjs/react-router-dom.min";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom/cjs/react-router-dom.min";
+import "./App.css";
 import Navbar from "./layout/Navbar";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -15,11 +13,11 @@ function App() {
         <Navbar />
         <div className="container">
           <Switch>
+            <Route exact path="/login">
+              <Login />
+            </Route>
             <Route exact path="/register">
               <Register />
-            </Route>
-            <Route path="/login">
-              <Login />
             </Route>
           </Switch>
         </div>
